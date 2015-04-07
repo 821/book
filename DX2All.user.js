225bfdb47d263737cf9290e96b13ca4111fb4c85
cunzhong <cunzhong@qq.com>
2015-04-07 18:33:01 +0800

    2015/04/07 18:33

--- DX2All.user.js
+++ DX2All.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name		DX2All
-// @version		1.1
+// @version		1.2
 // @include		*.com/bookDetail.jsp?dxNumber=*
 // @description		顯示 ssid ，通向 BK 查詢頁及 NB ZJK, JN ZJK, GZU ZJK 返回鏈獲取頁
 // @copyright		LCZ
 // @license		GPLv3
 // ==/UserScript==
 
-var version = "1.1";
-var date = "2015-3-16";
+var version = "1.2";
+var date = "2015-4-7";
 
 var getBK = location.href.replace("bookDetail.jsp?dxNumber=", "other.do?go=showmorelib&dxid=")
 var ssidfavDiv = document.getElementById("ssidfav");
 var ssid = ssidfavDiv.getAttribute("value");
 var divAdd = document.createElement("div");
-divAdd.innerHTML = ssid+' <a href="' + getBK + '&type=1">BK</a> <a href="http://cxbook.nlic.net.cn:8089/getbookread?BID='+ssid+'" target="_blank">NB</a>' + ' ' + '<a href="http://202.116.13.19:8080/GW/s/202.116.13.47/G.http-8088/getbookread?BID='+ssid+'" target="_blank">JN</a>' + ' ' + '<a href="http://210.40.8.52:4599/getbookread?BID='+ssid+'" target="_blank">GZ</a>';
+divAdd.innerHTML = ssid+' <a href="' + getBK + '&type=1">BK</a> <a href="http://cxbook.nlic.net.cn:8089/getbookread?BID='+ssid+'" target="_blank">NB</a> <a href="http://210.32.205.38:8088/getbookread?BID='+ssid+'" target="_blank">ZG</a> <a href="http://202.116.13.19:8080/GW/s/202.116.13.47/G.http-8088/getbookread?BID='+ssid+'" target="_blank">JN</a> <a href="http://210.40.8.52:4599/getbookread?BID='+ssid+'" target="_blank">GZ</a>';
 var grade1Div = document.getElementById("frameshow");
 var grade1Parent = grade1Div.parentNode;
 grade1Parent.insertBefore(divAdd, grade1Div);
--- Se2All.user.js
+++ Se2All.user.js
@@ -1,20 +1,37 @@
 // ==UserScript==
-// @name		DX2All
+// @name		Se2All
 // @version		1.2
-// @include		*.com/bookDetail.jsp?dxNumber=*
-// @description		顯示 ssid ，通向 BK 查詢頁及 NB, ZG, JN, GZU 等 ZJK 返回鏈獲取頁
+// @include		*/search?Field=*
+// @include		*/search?sw=*
+// @include		*/search?channel=search*
+// @description		顯示 ssid ，通向 BK 查詢頁及 NB ZJK, JN ZJK, GZU ZJK 返回鏈獲取頁
 // @copyright		LCZ
 // @license		GPLv3
 // ==/UserScript==
 
 var version = "1.2";
-var date = "2015-4-4";
+var date = "2015-4-7";
 
-var getBK = location.href.replace("bookDetail.jsp?dxNumber=", "other.do?go=showmorelib&dxid=")
-var ssidfavDiv = document.getElementById("ssidfav");
-var ssid = ssidfavDiv.getAttribute("value");
-var divAdd = document.createElement("div");
-divAdd.innerHTML = ssid+' <a href="' + getBK + '&type=1">BK</a> <a href="http://cxbook.nlic.net.cn:8089/getbookread?BID='+ssid+'" target="_blank">NB</a> <a href="http://210.32.205.38:8088/getbookread?BID='+ssid+'" target="_blank">ZG</a>' + ' ' + '<a href="http://202.116.13.19:8080/GW/s/202.116.13.47/G.http-8088/getbookread?BID='+ssid+'" target="_blank">JN</a>' + ' ' + '<a href="http://210.40.8.52:4599/getbookread?BID='+ssid+'" target="_blank">GZ</a>';
-var grade1Div = document.getElementById("frameshow");
-var grade1Parent = grade1Div.parentNode;
-grade1Parent.insertBefore(divAdd, grade1Div);
+Se2All = {
+	run: function() {
+		var book1Div = document.getElementsByClassName("book1");
+		var scoreDiv = document.getElementsByClassName("grade s1 hide");
+		for (var i=0; i<book1Div.length; i++) {
+			var ssidInput = document.getElementById("ssid"+i);
+			var ssid = ssidInput.getAttribute("value");
+			var urlInput = document.getElementById("url"+i);
+			var url = urlInput.getAttribute("value");
+			var getBK = url.replace("bookDetail.jsp?dxNumber=", "other.do?go=showmorelib&dxid=")
+			var divAdd = document.createElement("div");
+			divAdd.innerHTML =  ssid+' <a href="' + getBK + '&type=1">BK</a> <a href="http://cxbook.nlic.net.cn:8089/getbookread?BID='+ssid+'" target="_blank">NB</a> <a href="http://210.32.205.38:8088/getbookread?BID='+ssid+'" target="_blank">ZG</a> <a href="http://202.116.13.19:8080/GW/s/202.116.13.47/G.http-8088/getbookread?BID='+ssid+'" target="_blank">JN</a> <a href="http://210.40.8.52:4599/getbookread?BID='+ssid+'" target="_blank">GZ</a>';
+			var scoreParent = scoreDiv[i].parentNode;
+			scoreParent.insertBefore(divAdd, scoreDiv[i]);
+		}
+	}
+}
+
+try {
+	Se2All.run();
+}
+catch (e) {
+}
